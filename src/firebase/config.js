36047c1399fea14425f2a5e92b2706d431ec5a81
Kersti Miller <kersti.miller@bigbank.ee>
2023-01-17 17:12:28 +0200

    Install Firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyADxyXX4s7XHAW7s_-b3HuSnO9FUFKEMr4",
+  authDomain: "vue3-firebase-7ab61.firebaseapp.com",
+  projectId: "vue3-firebase-7ab61",
+  storageBucket: "vue3-firebase-7ab61.appspot.com",
+  messagingSenderId: "391467160236",
+  appId: "1:391467160236:web:cc099868fd5d11bbedc509"
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
+
+const projectFirestore = firebase.firestore()
+const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+export { projectFirestore, timestamp }
