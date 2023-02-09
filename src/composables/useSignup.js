fcf3691f96decc19fbc0cd0f4e64a9c2686caeae
Kersti Miller <kersti.miller@bigbank.ee>
2023-02-09 22:51:57 +0200

    Add useLogin composable and errorhandling

--- src/composables/useSignup.js
+++ src/composables/useSignup.js
@@ -8,6 +8,7 @@ const signup = async (email, password, displayName) => {
 
   try {
     const res = await projectAuth.createUserWithEmailAndPassword(email, password)
+
     if (!res) {
       throw new Error('Could not complete the signup')
     }
